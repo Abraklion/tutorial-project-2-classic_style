5651f990979d07f45fd4dda1e83cf9819add0651
Abraklion <abraklion@gmail.com>
2022-07-26 11:45:53 +0500

    Урок 2: "Ловим" скролл до конца страницы и модальное окно подарка
    Модифицировал: modals.js
    ТЗ пункты: 5,13

--- docs/script.js
+++ docs/script.js
@@ -148,8 +148,11 @@ const modals = () => {
   /**
    * МОДУЛЬ ДЛЯ РАБОТЫ С МОДАЛЬНЫМИ ОКНАМИ
    */
+  let btnPressed = false; // была ли нажата кнопка вызова модального окна
+
   function bindModal(triggerSelector, modalSelector, closeSelector) {
     let closeClickOverlay = arguments.length > 3 && arguments[3] !== undefined ? arguments[3] : true;
+    let triggerDestroy = arguments.length > 4 && arguments[4] !== undefined ? arguments[4] : false;
 
     /**
      *
@@ -157,6 +160,7 @@ const modals = () => {
      * modalSelector       -> селектор модального окна которое мы будем открывать
      * closeSelector       -> селектор который закрывает модальное окно
      * closeClickOverlay   -> буливо значения, можно ли скрывать модальне окно по клику подложку
+     * triggerDestroy      -> удалять селектор при клике на который открывается модальное окно
      *
      */
     const trigger = document.querySelectorAll(triggerSelector),
@@ -169,6 +173,7 @@ const modals = () => {
       // скрывает все модальные окна
       windows.forEach(item => {
         item.style.display = 'none';
+        item.classList.add('animated', 'fadeIn'); // из библиотеки animate.css
       });
     };
 
@@ -176,6 +181,12 @@ const modals = () => {
       item.addEventListener('click', e => {
         if (e.target) {
           e.preventDefault();
+        }
+
+        btnPressed = true;
+
+        if (triggerDestroy) {
+          item.remove();
         }
 
         hideAllModals();
@@ -202,6 +213,8 @@ const modals = () => {
 
   function showModalByTime(selector, time) {
     // показывает интересующию нас модалку через определенное время
+    // selector -> селектор модалки которую нужно открыть
+    // time     -> через сколько милисекунд открыть
     setTimeout(function () {
       let scroll = document.documentElement.offsetWidth > 991 ? Object(_core_calcScroll__WEBPACK_IMPORTED_MODULE_0__["calcScroll"])() : 0,
           display;
@@ -219,9 +232,26 @@ const modals = () => {
     }, time);
   }
 
+  function openByScroll(selector) {
+    // показывает нужную нам модалку когда пользователь доскролил до конца страницы
+    // selector -> селектор модалки которую нужно открыть
+    window.addEventListener('scroll', showModalScroll);
+
+    function showModalScroll() {
+      let scrollHeight = Math.max(document.documentElement.scrollHeight, document.body.scrollHeight);
+
+      if (!btnPressed && Math.ceil(window.scrollY + document.documentElement.clientHeight) >= scrollHeight) {
+        document.querySelector(selector).click();
+        window.removeEventListener('scroll', showModalScroll);
+      }
+    }
+  }
+
   bindModal('.button-design', '.popup-design', '.popup-design .popup-close');
   bindModal('.button-consultation', '.popup-consultation', '.popup-consultation .popup-close');
-  showModalByTime('.popup-consultation', 5000);
+  bindModal('.fixed-gift', '.popup-gift', '.popup-gift .popup-close', true, true);
+  openByScroll('.fixed-gift');
+  showModalByTime('.popup-consultation', 50000);
 };
 
 /* harmony default export */ __webpack_exports__["default"] = (modals);
--- src/js/modules/modals.js
+++ src/js/modules/modals.js
@@ -6,7 +6,9 @@ const modals = () => {
    * МОДУЛЬ ДЛЯ РАБОТЫ С МОДАЛЬНЫМИ ОКНАМИ
    */
 
-  function bindModal(triggerSelector, modalSelector, closeSelector, closeClickOverlay = true) {
+  let btnPressed = false; // была ли нажата кнопка вызова модального окна
+
+  function bindModal(triggerSelector, modalSelector, closeSelector, closeClickOverlay = true, triggerDestroy = false) {
 
     /**
      *
@@ -14,6 +16,7 @@ const modals = () => {
      * modalSelector       -> селектор модального окна которое мы будем открывать
      * closeSelector       -> селектор который закрывает модальное окно
      * closeClickOverlay   -> буливо значения, можно ли скрывать модальне окно по клику подложку
+     * triggerDestroy      -> удалять селектор при клике на который открывается модальное окно
      *
      */
 
@@ -28,6 +31,7 @@ const modals = () => {
 
       windows.forEach(item => {
         item.style.display = 'none';
+        item.classList.add('animated', 'fadeIn'); // из библиотеки animate.css
       });
 
     };
@@ -36,6 +40,12 @@ const modals = () => {
       item.addEventListener('click', (e) => {
         if (e.target) {
           e.preventDefault();
+        }
+
+        btnPressed = true;
+
+        if (triggerDestroy) {
+          item.remove();
         }
 
         hideAllModals();
@@ -68,6 +78,8 @@ const modals = () => {
   function showModalByTime(selector, time) {
 
     // показывает интересующию нас модалку через определенное время
+    // selector -> селектор модалки которую нужно открыть
+    // time     -> через сколько милисекунд открыть
 
     setTimeout(function() {
       let scroll = document.documentElement.offsetWidth > 991 ? calcScroll() : 0,
@@ -87,10 +99,34 @@ const modals = () => {
     }, time);
   }
 
+  function openByScroll(selector) {
+
+    // показывает нужную нам модалку когда пользователь доскролил до конца страницы
+    // selector -> селектор модалки которую нужно открыть
+
+    window.addEventListener('scroll', showModalScroll);
+
+    function showModalScroll(){
+      let scrollHeight = Math.max(document.documentElement.scrollHeight, document.body.scrollHeight);
+
+      if (!btnPressed && (Math.ceil(window.scrollY + document.documentElement.clientHeight) >= scrollHeight)) {
+
+        document.querySelector(selector).click();
+
+        window.removeEventListener('scroll', showModalScroll)
+
+      }
+    }
+
+  }
+
   bindModal('.button-design', '.popup-design', '.popup-design .popup-close');
   bindModal('.button-consultation', '.popup-consultation', '.popup-consultation .popup-close');
+  bindModal('.fixed-gift', '.popup-gift', '.popup-gift .popup-close', true, true);
 
-  showModalByTime('.popup-consultation', 5000);
+  openByScroll('.fixed-gift');
+
+  showModalByTime('.popup-consultation', 50000);
 };
 
 export default modals;
